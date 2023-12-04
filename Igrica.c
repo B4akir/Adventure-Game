026504ef129ed5441b8d2fc3625765361babd59b
Bakir Bukvic <baxoonb980@gmail.com>
2023-12-04 07:51:14 +0100

    Move select class into its own folder

--- Igrica.c
+++ Igrica.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "select_class.c"
 
 int onSelectedKlasa();
 int selectClass();
@@ -23,70 +23,4 @@ return 0;
 
 
 
- int selectClass(){
-
-
-  int selectedClass = 0;
-
-// beskonacna while petlja, iskljuci se kada je dobra selekcija
-
-//*pukne kada se uneste text umjesto broja*
-    while (1) { 
-
-
-
-
-//ISPIS=========================================================================
-
-        printf("Izaberi klasu:\n");
-        printf("  1) Vitez\n  2) Carobnjak\n  3) Lopov\n");
-
-        scanf("%d", &selectedClass);
-
-//ISPIS=========================================================================
-
-
-// iskljuci while petlju ako je dobra selekcija
-        if (selectedClass >= 1 && selectedClass <= 3) {
-            break; 
-            
-      
-        } else {
-            printf("Ne nudimo broj: %d, molimo izaberitie broj od 1-3\n",selectedClass);
-        }
-    }
-return  selectedClass;
-
-
-
- }
-
- int onSelectedKlasa(){
-
-int odabir=selectClass();
-
-    
- switch (odabir) {
-        case 1:
-            printf("\n Vitez si ");
-         
-            break;
-
-        case 2:
-             printf("\n Carobnjak  si ");
-          
-            break;
-
-        case 3:
-            printf("\n Lopov  si ");
-         
-            break;
-
-        default:
-          
-            printf("Neocekivana greska.\n");
-
-}
-
-return odabir;
- }
+ 
---
+++ select_class.c
@@ -0,0 +1,70 @@
+
+#include <stdio.h>
+
+int selectClass(){
+
+
+  int selectedClass = 0;
+
+// beskonacna while petlja, iskljuci se kada je dobra selekcija
+
+//*pukne kada se uneste text umjesto broja*
+    while (1) { 
+//*pukne kada se uneste text umjesto broja*
+
+
+
+//ISPIS=========================================================================
+
+        printf("Izaberi klasu:\n");
+        printf("  1) Vitez\n  2) Carobnjak\n  3) Lopov\n");
+
+        scanf("%d", &selectedClass);
+
+//ISPIS=========================================================================
+
+
+// iskljuci while petlju ako je dobra selekcija
+        if (selectedClass >= 1 && selectedClass <= 3) {
+            break; 
+            
+      
+        } else {
+            printf("Ne nudimo broj: %d, molimo izaberitie broj od 1-3\n",selectedClass);
+        }
+    }
+return  selectedClass;
+
+
+
+ }
+
+ int onSelectedKlasa(){
+
+int odabir=selectClass();
+
+    
+ switch (odabir) {
+        case 1:
+            printf("\n Vitez si ");
+         
+            break;
+
+        case 2:
+             printf("\n Carobnjak  si ");
+          
+            break;
+
+        case 3:
+            printf("\n Lopov  si ");
+         
+            break;
+
+        default:
+          
+            printf("Neocekivana greska.\n");
+
+}
+
+return odabir;
+ }
