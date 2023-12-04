0142cdcc5300f1040b3113f5359d1a69fc2996f0
Bakir Bukvic <baxoonb980@gmail.com>
2023-12-04 07:45:53 +0100

    Add functions for selecting a class

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,26 @@
+klasa2(){
+
+
+    
+//  switch (selectedClass) {
+//         case 1:
+//             printf("\n Vitez si ");
+         
+//             break;
+
+//         case 2:
+//              printf("\n Carobnjak  si ");
+          
+//             break;
+
+//         case 3:
+//             printf("\n Lopov  si ");
+         
+//             break;
+
+//         default:
+          
+//             printf("Neocekivana greska.\n");
+
+// }
+//  }
