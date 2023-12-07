00737bebeda7757505716c94f2c4e0b4dad33089
Bakir Bukvic <baxoonb980@gmail.com>
2023-12-07 09:18:44 +0100

    Put board in own file

--- Igrica.c
+++ Igrica.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "board.c"
 
 
 #define HEIGHT 10
 #define WIDTH 20
 
-
-
-
-typedef struct {
-    int x,y;
-
-}Player;
 
  void boardCreation(Player *player);
  void movement(Player *player);
@@ -38,68 +31,3 @@ return 0;
 
 
 
-void boardCreation(Player *player)
-{
-
-
-
-system ("cls");
-
-for (int i=0;i<HEIGHT;i++){
-    
-   
-for (int j=0;j<WIDTH;j++){
-    
-    if (i==player->y && j==player->x){
-        printf("@");
-    }
-    else{
-        printf(".");
-    }
-}
- printf("\n");
-}
-
-
-
-
-
-
-}
-
-
-
-
-void initPlayer(Player *player){
-
-player->y= HEIGHT/2;
-player->x= WIDTH/2;
-
-
-};
-
-
-void movement(Player *player){
-
-char unos;
-
-printf("---> \n");
-scanf("%c",&unos);
-
-switch (unos){
-    case 'w':
-    player->y--;
-    break;
-case 's':
-    player->y++;
-    break;
-case 'd':
-    player->x++;
-    break;
-case 'a':
-    player->x--;
-    break;
-
-}
-
-}
