ecc0511a330211429739fb3f4df5ea5c9e7e8f87
Bakir Bukvic <baxoonb980@gmail.com>
2023-12-15 18:16:12 +0100

    Add all hodnjiks to new map

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <Windows.h>
+
+
+#define HODNJIK 34
+#define VRATA 15
+#define ZID 186
+#define PLAFON 205
+
+#define LIJEVIG 201
+#define DESNIG 187
+#define LIJEVID 200
+#define DESNID 188
+
+
+
+
+#define HEIGHT 40
+#define WIDTH 134
+
 
 typedef struct {
-    char data[50][50];
+    char data[HEIGHT][WIDTH];
 } World;
 
 typedef struct {
@@ -22,6 +41,10 @@ void regionInit(Region *region, int startY, int startX, int height, int width, c
 
 
 
+
+
+
+
 typedef struct {
     int x,y;
 
@@ -37,18 +60,40 @@ typedef struct {
 
 
 
+
+   Region regions[8];
+
+
+
 void initWorld(World *world) {
-    for (int i = 0; i < 50; i++) {
-        for (int j = 0; j < 50; j++) {
-            world->data[i][j] = ' ';
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+
+            if (i==HEIGHT-1 && j==WIDTH-1){
+                world->data[i][j] = 'X';
+            }
+
+            else if (i==0 && j==0){
+                world->data[i][j] = 'X';
+            }
+           else  if (i==0 && j==WIDTH-1){
+                world->data[i][j] = 'X';
+            }
+           else  if (i==HEIGHT-1 && j==0){
+                world->data[i][j] = 'X';
+            }
+            else{
+                world->data[i][j] = ' ';
+            }
+       
         }
     }
 }
 
 void ispis(World *world) {
     system("cls");
-    for (int i = 0; i < 50; i++) {
-        for (int j = 0; j < 50; j++) {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
             printf("%c", world->data[i][j]);
         }
         printf("\n");
@@ -56,26 +101,37 @@ void ispis(World *world) {
 }
 
 void regionIntoWorld(Region *region, World *world) {
-    for (int i = 0; i < 50; i++) {
-        for (int j = 0; j < 50; j++) {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
             if ((i >= region->startY && i <= region->startY + region->height) &&
                 (j >= region->startX && j <= region->startX + region->width)) {
-                // Check if on the edge
-                if (i == region->startY || i == region->startY + region->height ||
-                    j == region->startX || j == region->startX + region->width) {
-                    world->data[i][j] = '+';
-
-
-                    
-                } 
-                
-                
-                
+                // Check if on the right upper edge
+                if (i == region->startY && j == region->startX + region->width) {
+                    world->data[i][j] = DESNIG;
+                }
+                // Check if on the left upper edge
+                else if (i == region->startY && j == region->startX) {
+                    world->data[i][j] = LIJEVIG;
+                }
+                // Check if on the bottom right edge
+                else if (i == region->startY + region->height && j == region->startX + region->width) {
+                    world->data[i][j] = DESNID;
+                }
+                // Check if on the bottom left edge
+                else if (i == region->startY + region->height && j == region->startX) {
+                    world->data[i][j] = LIJEVID;
+                }
+                // Check if on the vertical lines not containing edges
+                else if ((j == region->startX || j == region->startX + region->width) && (i != region->startY && i != region->startY + region->height)) {
+                    world->data[i][j] = ZID;
+                }
+                // Check if on the horizontal lines not containing edges
+                else if ((i == region->startY || i == region->startY + region->height) && (j != region->startX && j != region->startX + region->width)) {
+                    world->data[i][j] = PLAFON;
+                }
                 else {
                     world->data[i][j] = region->fillCharacter;
                 }
-           
-               
             }
         }
     }
@@ -83,27 +139,150 @@ void regionIntoWorld(Region *region, World *world) {
 
 
 
-void doors(Region *region, World *world){
 
-//make a function that puts a "#" at each regions wall middle point to make doors
+
+ 
+
+void doorL(Region *region, World *world,int index){
+    Region *region0 = &regions[index];
+
+    int y = region0->startY;
+    int x = region0->startX;
+    int height = region0->height;
+
+ 
+
+ 
+        int i=y+height/2;
+
+        
+        world->data[i][x] = VRATA;
+
+
+
 }
+
+void doorR(Region *region, World *world, int index ){
+
+   Region *region0 = &regions[index];
+
+    int y = region0->startY;
+    int x = region0->startX;
+    int height = region0->height;
+    int width = region0->width;
+
+ 
+        int i=y+height/2;
+        int j=x+width;
+
+        world->data[i][j] = VRATA;
+
+
+
+
+
+}
+
+void doorT(Region *region, World *world, int index){
+
+   Region *region0 = &regions[index];
+
+    int y = region0->startY;
+    int x = region0->startX;
+    int width = region0->width;
+
+
+ 
+    int j=x+width/2;
+
+
+    world->data[y][j] = VRATA;
+
+
+
+}
+
+void doorB(Region *region, World *world, int index){
+
+ Region *region0 = &regions[index];
+
+    int y = region0->startY;
+    int x = region0->startX;
+    int height = region0->height;
+    int width = region0->width;
+ 
+
+ 
+    int j=x+width/2;
+    int i=y+height;
+
+    world->data[i][j] = VRATA;
+
+
+
+}
+
+
+//  #define HEIGHT 40
+// #define WIDTH 134
+
 
 
 
 void initializeRegions(World *world) {
-    Region regions[6];
+    regionInit(&regions[0], /* y*/30,   /* x*/ 60, /* height*/ 7,  /* width*/  21, '.');
+     regionInit(&regions[1], 30, 89, 5, 12, '1');
+     regionInit(&regions[2], 22, 90, 4, 20, '2');
+     regionInit(&regions[3], 22, 50, 4, 25, '3');
+     regionInit(&regions[4], 18, 17, 4, 8, '4');
+     regionInit(&regions[5], 25, 8, 5, 15, '5');
+     regionInit(&regions[6], 33, 12, 5, 5, '6');
+   
 
-    regionInit(&regions[0], 0, 0, 7, 10, '.');
-    regionInit(&regions[1], 1, 30, 4, 12, '1');
-    regionInit(&regions[2], 12, 1, 5, 15, '2');
-    regionInit(&regions[3], 25, 5, 5, 10, '3');
-    regionInit(&regions[4], 10, 30, 5, 10, '4');
-    regionInit(&regions[5], 25, 30, 5, 5, '5');
-
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 7; i++) {
         regionIntoWorld(&regions[i], world);
-        doors(&regions[i], world);  // Call the doors function for each region
     }
+}
+
+
+void initializeDoors(World *world, Region *region) {
+    
+    
+    //regija 0
+    doorR(region, world, 0);
+    doorL(region, world, 0);
+
+
+// regija 1
+    doorL(region, world, 1);
+    doorT(region, world, 1);
+
+//regija 2
+    doorB(region, world, 2);
+    doorL(region, world, 2);
+
+
+//regija 3
+doorR(region, world, 3);
+doorT(region, world, 3);
+doorL(region, world, 3);
+
+
+
+//regija 4
+    doorR(region, world, 4);
+    doorB(region, world, 4);
+
+
+//regija 5
+doorT(region, world, 5);
+doorB(region, world, 5);
+
+
+//regia 6
+doorT(region, world, 6);
+
+
 }
 
 
@@ -113,9 +292,57 @@ void initializeRegions(World *world) {
 
 
 
+hodnjikH(int x, int y, int z , World *world){
+
+//x je pocetak hodnika
+//y je visina hodnjika
+//z je intezitet hodnjika (duzina)
+
+for (int i=0; i<HEIGHT; i++){
+    for (int j=0; j<WIDTH; j++){
+       if (y==i && j>=x && j<=z+x){
+           world->data[i][j]=HODNJIK;
+     
+    
+
+}
+}
+}
+}
 
 
 
+hodnjikV(int x, int y, int z , World *world){
+
+//x je pocetak hodnika
+//y je visina hodnjika
+//z je intezitet hodnjika (duzina)
+
+for (int i=0; i<HEIGHT; i++){
+    for (int j=0; j<WIDTH; j++){
+       if (x==j && i>=y && i<=z+y){
+           world->data[i][j]=HODNJIK;
+     
+    
+
+}
+}
+}
+}
+
+
+
+
+hodnjikInit(World *world){
+    // hodnjikH(11,4,17,world);
+    // hodnjikH(11+16,3,2,world);
+
+    // hodnjikV(36,7,2,world);
+    
+
+
+
+}
 
 
 
@@ -131,12 +358,18 @@ int main() {
 
     initWorld(&world);
     initializeRegions(&world);
-   
+    initializeDoors(&world, &region);
+    hodnjikInit(&world);
+
+ 
 
     ispis(&world);
-
 int x;
-    scanf("%d",x);
+
+
+
+
+    scanf("%d",&x);
    
 
 
